a469bb587f0d4524adc3a3d6599044c55d5b3c7b
null <null>
2023-09-18 11:42:05 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace FormComplejos_G2_2024_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbR1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbIm1 = new System.Windows.Forms.TextBox();
+            this.txtbR2 = new System.Windows.Forms.TextBox();
+            this.txtbIm2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbSuma1 = new System.Windows.Forms.Label();
+            this.btnSuma1 = new System.Windows.Forms.Button();
+            this.btnConjugar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbR1
+            // 
+            this.txtbR1.Location = new System.Drawing.Point(58, 95);
+            this.txtbR1.Name = "txtbR1";
+            this.txtbR1.Size = new System.Drawing.Size(100, 20);
+            this.txtbR1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Complejo 1";
+            // 
+            // txtbIm1
+            // 
+            this.txtbIm1.Location = new System.Drawing.Point(206, 95);
+            this.txtbIm1.Name = "txtbIm1";
+            this.txtbIm1.Size = new System.Drawing.Size(100, 20);
+            this.txtbIm1.TabIndex = 0;
+            // 
+            // txtbR2
+            // 
+            this.txtbR2.Location = new System.Drawing.Point(425, 95);
+            this.txtbR2.Name = "txtbR2";
+            this.txtbR2.Size = new System.Drawing.Size(100, 20);
+            this.txtbR2.TabIndex = 0;
+            // 
+            // txtbIm2
+            // 
+            this.txtbIm2.Location = new System.Drawing.Point(573, 95);
+            this.txtbIm2.Name = "txtbIm2";
+            this.txtbIm2.Size = new System.Drawing.Size(100, 20);
+            this.txtbIm2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(541, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Complejo 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(312, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "i";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(354, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "op";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(679, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "i";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(713, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "=";
+            // 
+            // lbSuma1
+            // 
+            this.lbSuma1.AutoSize = true;
+            this.lbSuma1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSuma1.Location = new System.Drawing.Point(757, 98);
+            this.lbSuma1.Name = "lbSuma1";
+            this.lbSuma1.Size = new System.Drawing.Size(55, 24);
+            this.lbSuma1.TabIndex = 6;
+            this.lbSuma1.Text = "0 + 0i";
+            // 
+            // btnSuma1
+            // 
+            this.btnSuma1.Location = new System.Drawing.Point(68, 149);
+            this.btnSuma1.Name = "btnSuma1";
+            this.btnSuma1.Size = new System.Drawing.Size(75, 23);
+            this.btnSuma1.TabIndex = 7;
+            this.btnSuma1.Text = "Sumar";
+            this.btnSuma1.UseVisualStyleBackColor = true;
+            this.btnSuma1.Click += new System.EventHandler(this.btnSuma1_Click);
+            // 
+            // btnConjugar
+            // 
+            this.btnConjugar.Location = new System.Drawing.Point(217, 148);
+            this.btnConjugar.Name = "btnConjugar";
+            this.btnConjugar.Size = new System.Drawing.Size(75, 23);
+            this.btnConjugar.TabIndex = 8;
+            this.btnConjugar.Text = "Conjugar";
+            this.btnConjugar.UseVisualStyleBackColor = true;
+            this.btnConjugar.Click += new System.EventHandler(this.btnConjugar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(481, 148);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.TabIndex = 9;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Location = new System.Drawing.Point(359, 149);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(75, 23);
+            this.btnRestar.TabIndex = 10;
+            this.btnRestar.Text = "Restar";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 450);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnConjugar);
+            this.Controls.Add(this.btnSuma1);
+            this.Controls.Add(this.lbSuma1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbIm2);
+            this.Controls.Add(this.txtbR2);
+            this.Controls.Add(this.txtbIm1);
+            this.Controls.Add(this.txtbR1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbR1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbIm1;
+        private System.Windows.Forms.TextBox txtbR2;
+        private System.Windows.Forms.TextBox txtbIm2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbSuma1;
+        private System.Windows.Forms.Button btnSuma1;
+        private System.Windows.Forms.Button btnConjugar;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnRestar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FormComplejos_G2_2024_1
+{
+    public partial class Form1 : Form
+    {
+        Complejo c1;
+        Complejo c2;
+        Complejo c3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma1_Click(object sender, EventArgs e)
+        {
+            c1 = new Complejo(float.Parse(txtbR1.Text), float.Parse(txtbIm1.Text));
+            c2 = new Complejo(float.Parse(txtbR2.Text), float.Parse(txtbIm2.Text));
+            c3 = c1 + c2;
+            lbSuma1.Text = c3.ToString();
+        }
+
+        private void btnConjugar_Click(object sender, EventArgs e)
+        {
+            lbSuma1.Text = (~c3).ToString(); 
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            c1 = new Complejo(float.Parse(txtbR1.Text), float.Parse(txtbIm1.Text));
+            c2 = new Complejo(float.Parse(txtbR2.Text), float.Parse(txtbIm2.Text));
+            c3 = c1 - c2;
+            lbSuma1.Text = c3.ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            c1 = new Complejo(float.Parse(txtbR1.Text), float.Parse(txtbIm1.Text));
+            c2 = new Complejo(float.Parse(txtbR2.Text), float.Parse(txtbIm2.Text));
+            c3 = c1 * c2;
+            lbSuma1.Text = c3.ToString();
+        }
+    }
+}
